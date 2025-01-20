bfdca35f4148a3f726c672442cbe632a9d420e8b
slafniy <slafniy@gmail.com>
2025-01-20 23:16:08 +0400

    added open_toolkit.bat

---
+++ open_toolkit.bat
@@ -0,0 +1,5 @@
+set project=PPMeter
+set toolkit_path="D:\SteamLibrary\steamapps\common\Baldurs Gate 3 Toolkit\"
+
+cd /D %toolkit_path%
+%toolkit_path%\Glasses.exe -project %project% -lvl Basic_Level_A
